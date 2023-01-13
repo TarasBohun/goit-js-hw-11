246c80668f842fd55bdd2e0b0ab54692246d0771
Taras <tarasbohun07@gmail.com>
2023-01-13 15:06:05 +0200

    add pagination

--- src/index.js
+++ src/index.js
@@ -3,69 +3,73 @@ import renderMarkup from './renderMarkup';
 import './css/styles.css';
 
 const searchForm = document.querySelector('.search-form');
-const input = document.querySelector('input');
-const gallery = document.querySelector('.gallery');
-const body = document.querySelector('body');
-const container = document.querySelector('.container');
-const btnLoadMore = document.querySelector('.btnLoadMore');
-// console.log(searchForm);
+const searchBtn = document.querySelector('.search-btn');
+// const container = document.querySelector('.container');
+const containerGallery = document.querySelector('.container-gallery');
 
 searchForm.addEventListener('submit', onSearch);
 
 let searchQuery = '';
-let page = 1;
-const perPage = 5;
-
-// btnLoadMore.classList('visuallyhidden') = 'false';
+let page = null;
+let gallery = '';
+const perPage = 3;
+let btnLoadMore = '';
 
 function onSearch(e) {
   e.preventDefault();
   searchQuery = e.currentTarget.elements.searchQuery.value;
 
+  searchBtn.disabled = true;
+
+  page = 1;
+
+  containerGallery.innerHTML = markupGallery();
+  gallery = document.querySelector('.gallery');
+
   if (searchQuery === '') {
     return;
   }
 
-  searchPictures();
+  searchAddPictures();
 
-  // if (container.insertAdjacentHTML('beforeend', addLoadMoreBtn())) {
-  //   return;
-  // }
-
-  container.insertAdjacentHTML('beforeend', addLoadMoreBtn());
-
-  const btnLoadMore = document.querySelector('.btnLoadMore');
-  btnLoadMore.addEventListener('click', onLoadMore);
-
-  // perPage += 1;
-
-  clearPictures();
+  setTimeout(() => {
+    createLoadMoreBtn();
+  }, 500);
 }
 
-// renderMarkup(images);
-
-function searchPictures() {
+function searchAddPictures() {
   fetchPictures(searchQuery, page, perPage)
     .then(data => {
       gallery.insertAdjacentHTML('beforeend', renderMarkup(data.hits));
     })
     .then(data => {
       page += 1;
+      searchBtn.disabled = false;
+      btnLoadMore.disabled = false;
     })
     .catch(err => console.log(err));
 }
 
-function addLoadMoreBtn() {
-  return `<button class='btnLoadMore' type="button">Load More</button>`;
+function createLoadMoreBtn() {
+  if (page !== 2) {
+    return;
+  }
+
+  gallery.insertAdjacentHTML('afterend', markupLoadMoreBtn());
+
+  btnLoadMore = document.querySelector('.btnLoadMore');
+  btnLoadMore.addEventListener('click', onLoadMore);
 }
 
-function clearPictures() {
-  gallery.innerHTML = '';
-  // perPage = 0;
+function markupLoadMoreBtn() {
+  return `<div class='container-btn'><button class='btnLoadMore is-hidden' type="button">Load More</button></div>`;
 }
 
-// btnLoadMore.addEventListener('click', onLoadMore);
+function markupGallery() {
+  return `<div class="gallery"></div>`;
+}
 
 function onLoadMore() {
-  searchPictures();
+  searchAddPictures();
+  btnLoadMore.disabled = true;
 }
